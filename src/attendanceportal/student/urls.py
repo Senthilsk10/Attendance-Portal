282494ce15cf9c5351b2cbfc0c06b13c9f63d6b7
Senthil Kumaran <legendsenthilsk10@gmail.com>
2023-12-02 23:40:21 +0530

    added route for adding presence to a pool
    added a button to mark the students presence to a given pool next we have to
    
    hide or show that button to user bases on the conditions they are able to able to mark presence or not with ipcheck and reeducing the duplicate entry into same pool and location acces

--- src/attendanceportal/student/urls.py
+++ src/attendanceportal/student/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import student,pool_view
+from .views import student,pool_view,get_attendance
 
 urlpatterns = [
     path('profile/',student.as_view(),name = "student"),
-    path('view/pool/<int:pk>/',pool_view.as_view(),name = "student_pool_view")
+    path('view/pool/<int:pk>/',pool_view.as_view(),name = "student_pool_view"),
+    path("markPresence/",get_attendance,name = "mark_presence"),
 ]
--- src/attendanceportal/student/views.py
+++ src/attendanceportal/student/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from user.models import User,attendance_pool,subject
+from django.shortcuts import render,redirect
+from user.models import User,attendance_pool,subject,attendance
 from django.views import View
+from django.http import JsonResponse
 # Create your views here.
 class student(View):
     def get(self, request, *args, **kwargs):
@@ -15,7 +16,26 @@ class pool_view(View):
     def get(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
         current_pool = attendance_pool.objects.get(id = pk)
+        result_data = attendance.objects.filter(pool = current_pool)
         template_name = "student_pool_view.html"
-        return render(request,template_name, {"pool":current_pool})
+        return render(request,template_name, {"pool":current_pool,"result_data":result_data})
 
 
+def get_attendance(request):#exception arises as anonymous user userid doesnot exists so handle it
+    if request.method == 'POST':
+        id = request.POST.get('pool_id')
+        user_pool = attendance_pool.objects.get(id = id)
+        student = request.user
+        student_rollno = student.userid
+        attendance_status = "Present"
+
+        attendance_data = attendance(
+            pool = user_pool,
+            user = student,
+            student_roll = student_rollno,
+            status = attendance_status
+        )
+
+        attendance_data.save()
+
+        return JsonResponse("success",status = 200,safe=False)
