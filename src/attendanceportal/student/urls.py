12d5330c512d403932b1d0751a76cf4bbb80bde3
Senthil Kumaran <legendsenthilsk10@gmail.com>
2023-12-02 13:22:17 +0530

    added pool view
    added pool view redirection from the students profile and we have to add the present input feaure and wrap up by adding teacher view after that

--- src/attendanceportal/student/urls.py
+++ src/attendanceportal/student/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import student
+from .views import student,pool_view
 
 urlpatterns = [
     path('profile/',student.as_view(),name = "student"),
+    path('view/pool/<int:pk>/',pool_view.as_view(),name = "student_pool_view")
 ]
--- src/attendanceportal/student/views.py
+++ src/attendanceportal/student/views.py
@@ -5,5 +5,17 @@ from django.views import View
 class student(View):
     def get(self, request, *args, **kwargs):
         user_id = request.user.userid
+        user_dept = request.user.dept
+        user_sem = request.user.sem
+        current_pools =attendance_pool.objects.filter(subject__dept = user_dept,subject__sem = user_sem)
         template_name = "student.html"
-        return render(request,template_name, {"user_id":user_id})
+        return render(request,template_name, {"user_id":user_id,"pools":current_pools})
+
+class pool_view(View):
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        current_pool = attendance_pool.objects.get(id = pk)
+        template_name = "student_pool_view.html"
+        return render(request,template_name, {"pool":current_pool})
+
+
